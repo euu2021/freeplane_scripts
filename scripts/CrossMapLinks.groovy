6b4185da8888f0e1eae465c242a3dabda26e36c8
kauen <77707706+euu2021@users.noreply.github.com>
2026-07-26 15:37:05 -0300

    Canonize the short licence notice: SPDX line only
    Five scripts carried the four-line "This program is free software..." notice
    on top of the SPDX identifier. The identifier says the same thing, and the
    full text is in LICENSE, so the notice only made the headers differ from each
    other. Every script now has the same four-line header: copyright, SPDX,
    discussion thread (when there is one), version.

--- scripts/CrossMapLinks.groovy
+++ scripts/CrossMapLinks.groovy
@@ -1,9 +1,5 @@
 // Copyright (C) 2026  euu2021 (Github)
 // SPDX-License-Identifier: GPL-2.0-or-later
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 2 of the License, or
-// (at your option) any later version.
 // Discussion thread: https://github.com/freeplane/freeplane/issues/2938
 // Version: 1.0.0
 
