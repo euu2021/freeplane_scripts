79fcd94b0ad4404561a8edb9a8a4adf0bca4c403
kauen <77707706+euu2021@users.noreply.github.com>
2026-07-27 18:38:42 -0300

    FoldedNodeCount: link the discussion thread in the header [no-bump]

--- scripts/FoldedNodeCount.groovy
+++ scripts/FoldedNodeCount.groovy
@@ -1,5 +1,6 @@
 // Copyright (C) 2026  euu2021 (Github)
 // SPDX-License-Identifier: GPL-2.0-or-later
+// Discussion thread: https://github.com/freeplane/freeplane/discussions/2956
 // Version: 1.0
 
 /*
